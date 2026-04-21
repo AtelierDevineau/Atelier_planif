b7751fb79bd47f85e1a7efb7395ef915e418bd2d
AtelierDevineau <decor@atelierdevineau.com>
2026-04-21 15:24:28 +0200

    Add files via upload

---
+++ App.py
@@ -0,0 +1,37 @@
+import streamlit as st
+from streamlit_calendar import calendar
+st.title("Planification projets")
+
+"Liste des projets (à lier à excel)"
+Projets = [
+    {
+     "Nom" : "Enlèvement au sérail",
+     "Début" : "2026-01-01",
+     "Fin" : "2026-05-12"
+     },
+    {
+     "Nom" : "Manon Lescaut",
+     "Début" : "2026-02-03",
+     "Fin" : "2026-10-16"
+     },
+    {
+     "Nom" : "Brundibar",
+     "Début" : "2026-03-02",
+     "Fin" : "2026-05-15"
+     }
+    ]
+
+# Options calendrier
+options = {
+    "initialView": "dayGridMonth",
+    "locale": "fr",
+    "headerToolbar": {
+        "left": "prev,next today",
+        "center": "title",
+        "right": "dayGridMonth,timeGridWeek,timeGridDay"
+    }
+}
+
+
+# Affichage du calendrier
+calendar(events = Projets, options = options)
