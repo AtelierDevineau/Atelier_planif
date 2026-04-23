a64c2f17321ea67439c0f98fd9913eb247a217f0
AtelierDevineau <decor@atelierdevineau.com>
2026-04-23 11:47:13 +0200

    Update App.py

--- App.py
+++ App.py
@@ -117,7 +117,8 @@ with Assignation:
    
     #Choix du projet
     Choix_projet = st.menu_button("Choisir un projet :", options=[p["Nom"] for p in Projets])
-    While (Choix_projet != None):
+    
+    While Choix_projet != None:
         st.header(Choix_projet)  
           #Sauvegarde des données
         if Choix_projet not in st.session_state.Data_proj:
