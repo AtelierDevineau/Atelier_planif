0ead2e1ec291bf9b67cebb2d683cd1f4bb3589a8
AtelierDevineau <decor@atelierdevineau.com>
2026-04-24 12:04:18 +0200

    Add calendrier_tab function for calendar display

---
+++ Calendrier.py
@@ -0,0 +1,21 @@
+import streamlit as st
+from streamlit_calendar import calendar
+from Data import Projets_cal, Absences_cal, options_calendrier
+
+def calendrier_tab():
+  """Affiche onglet calendrier"""
+  st.header('Calendrier')
+      
+      # Choix entre absence ou projet
+      selection = st.pills(
+      " ",
+      ["Projets","Absences"],
+      selection_mode="single",
+      default = "Projets"
+      )
+      if selection == "Projets" :
+          # Affichage du calendrier projet
+          calendar(events = Projets_cal, options = options_calendrier)
+      if selection == "Absences":
+          calendar(events = Absences_cal, options = options_calendrier)
+   
