42593c86867e99f497741fc147a1e6b71520e172
AtelierDevineau <decor@atelierdevineau.com>
2026-04-24 15:10:07 +0200

    Update Calendrier.py

--- Calendrier.py
+++ Calendrier.py
@@ -5,17 +5,16 @@ from Data import Projets_cal, Absences_cal, options_calendrier
 def calendrier_tab():
   """Affiche onglet calendrier"""
   st.header('Calendrier')
-      
-      # Choix entre absence ou projet
-      selection = st.pills(
-      " ",
-      ["Projets","Absences"],
-      selection_mode="single",
-      default = "Projets"
-      )
-      if selection == "Projets" :
-          # Affichage du calendrier projet
-          calendar(events = Projets_cal, options = options_calendrier)
-      if selection == "Absences":
-          calendar(events = Absences_cal, options = options_calendrier)
+  # Choix entre absence ou projet
+  selection = st.pills(
+  " ",
+  ["Projets","Absences"],
+  selection_mode="single",
+  default = "Projets"
+  )
+  if selection == "Projets" :
+    # Affichage du calendrier projet
+    calendar(events = Projets_cal, options = options_calendrier)
+  if selection == "Absences":
+    calendar(events = Absences_cal, options = options_calendrier)
    
