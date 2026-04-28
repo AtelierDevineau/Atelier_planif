4cc8cf566b29c0165bb7ae2dff39bd8eba4cf725
AtelierDevineau <decor@atelierdevineau.com>
2026-04-28 16:19:33 +0200

    Update Crea_proj.py

--- Crea_proj.py
+++ Crea_proj.py
@@ -5,11 +5,9 @@ def crea_proj_tab():
   """Affiche l'outil de création de projet"""
   st.subheader('Création de projet')
   nom_proj = st.text_input("Entrez le nom du projet")
-  date_debut =st.date_input(label="Début du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
-  date_fin =st.date_input(label="Fin du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
   
   col1, col2 = st.columns(2)
   with col1:
-    date_debut =st.date_input(label="Début du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
+    date_debut =st.date_input("Début du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
   with col2:
-    date_fin =st.date_input(label="Fin du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
+    date_fin =st.date_input("Fin du projet", format="YYYY/MM/DD", label_visibility="visible", width="stretch")
